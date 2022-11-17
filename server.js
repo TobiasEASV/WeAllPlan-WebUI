192110ed30e505c8e6b37a34433c642b14628804
Tobias Rasmussen <tobi9782@easv365.dk>
2022-11-17 15:34:20 +0100

    Håber dette virker!!!!

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/we-all-plan-web-ui'))
 //app.use(nocache())
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/dist/we-all-plan-web-ui/index.html')
+  res.sendFile(__dirname + '//dist/we-all-plan-web-ui/index.html')
 });
 app.listen(process.env.PORT || port, () => {
   console.log("Server is listening on port "+port);

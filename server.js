96f678a6d6c004222fbcccc3f57eccc9ba5b70b3
Tobias Rasmussen <tobi9782@easv365.dk>
2022-11-17 15:24:10 +0100

    Håber dette virker

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const port = 8080;
+const app = express();
+//const nocache = require("nocache");
+
+
+app.use(express.static(__dirname + '/dist/we-all-plan-web-ui'))
+//app.use(nocache())
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/we-all-plan-web-ui/index.html')
+});
+app.listen(process.env.PORT || port, () => {
+  console.log("Server is listening on port "+port);
+});

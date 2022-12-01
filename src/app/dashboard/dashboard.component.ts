321b3748630e0540a3eee5b09c7e24b12324f6e6
Theut94 <89963626+Theut94@users.noreply.github.com>
2022-12-01 22:45:09 +0100

    Dashboard first setup

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import {HttpService} from "../../services/http.service";
 import {Event} from "../types/event";
 import {ActivatedRoute} from "@angular/router";
+import {User} from "../types/user";
 
 
 let Event: Event[];
+let User:User;
 
 @Component({
   selector: 'app-dashboard',
@@ -14,10 +16,12 @@ let Event: Event[];
 export class DashboardComponent implements OnInit {
 
   Events : Event[] = Event;
+  User: User = User;
   constructor(public http: HttpService,private route: ActivatedRoute) { }
 
   async ngOnInit(): Promise<void> {
     this.Events = this.route.snapshot.data['Event'];
+    this.User = this.http.user
   }
 
 }

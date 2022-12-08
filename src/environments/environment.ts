d09117a657d7597d2ec932418ed88229b06a1123
Theut94 <89963626+Theut94@users.noreply.github.com>
2022-12-08 20:04:20 +0100

    Almost Working with name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   baseUrl: 'https://localhost:7158/',
   production: false,
   googleClientId: '551110707011-7e3nd3brmonrold8o0pdp1i1bhc3djr6.apps.googleusercontent.com',
-  baseDomainUrl: "https://localhost:4200/"
+  baseDomainUrl: "localhost:4200/"
 };
 
 /*

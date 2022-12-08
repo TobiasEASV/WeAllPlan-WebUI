51fbe01dbbb5cf40ef4dd965914707d2722ff4ff
JanWohlgehagen <jan.wohlgehagen@gmail.com>
2022-12-08 10:31:53 +0100

    Home page done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpService} from "../../services/http.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,16 @@ import {HttpService} from "../../services/http.service";
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public http: HttpService) { }
+  constructor(public http: HttpService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  TryWAP() {
+    if (this.http.IsUser){
+      this.router.navigate(['CreateEvent']);
+    } else {
+      this.router.navigate(['Login']);
+    }
+  }
 }

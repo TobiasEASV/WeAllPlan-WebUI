37e08328ed9c065475c37d26c8e972ca17aaad40
JanWohlgehagen <65564154+JanWohlgehagen@users.noreply.github.com>
2022-12-12 13:55:59 +0100

    Update environment.prod.ts (#44)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  baseUrl: 'https://localhost:7158/',
+  production: true,
+  googleClientId: '551110707011-7e3nd3brmonrold8o0pdp1i1bhc3djr6.apps.googleusercontent.com',
+  baseDomainUrl: "localhost:4200/"
 };
